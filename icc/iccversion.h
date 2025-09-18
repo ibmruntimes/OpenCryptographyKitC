f55d94903e04b31185cd6955e40fe67438e95e01
smcmahonibm <smcmahon@au1.ibm.com>
2025-09-18 14:13:59 +1000

    Remaining updates

--- icc/iccversion.h
+++ icc/iccversion.h
@@ -1,14 +1,13 @@
 /*************************************************************************
-// Copyright IBM Corp. 2023
-//
-// Licensed under the Apache License 2.0 (the "License"). You may not use
-// this file except in compliance with the License. You can obtain a copy
-// in the file LICENSE in the source distribution.
+ Copyright IBM Corp. 2023
+
+ Licensed under the Apache License 2.0 (the "License"). You may not use
+ this file except in compliance with the License. You can obtain a copy
+ in the file LICENSE in the source distribution.
 *************************************************************************/
 
 /*************************************************************************
-// Description: ICC Version
-//
+ Description: ICC Version
 *************************************************************************/
 
 #ifndef INCLUDED_ICCVERSION
